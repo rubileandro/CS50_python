cf094cc3bb76be0bc9dc0f991c23eec6266e1957
Rubi Leandro <rubi@brislandventures.com>
2023-07-19 08:28:27 +0400

    Remove whitespace and capitalize

--- hello.py
+++ hello.py
@@ -1,1 +1,22 @@
-print("hello, world")
+# Option 1
+# # What is your name?
+# name = input("What's your name? ")
+
+# # Remove whitespace & capitalize name
+# name = name.strip().title()
+
+# # Greet user
+# #print("Hello,", name, sep="???")
+# print(f"Hello, {name}")
+
+# Option 2
+# get name, remove whitepsace and capitalize
+name = input("What's your name?").strip().title()
+
+# Greet user
+#print("Hello,", name, sep="???")
+print(f"Hello, {name}")
+
+# # overide default value \n
+# print("Hello,", end="")
+# print(name)
