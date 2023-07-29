7539afc1d905462912b695501c3f6ad624253115
Rubi Leandro <rubi@brislandventures.com>
2023-07-29 13:08:57 +0400

    using return

--- calculator.py
+++ calculator.py
@@ -14,10 +14,22 @@
 
 # print(z)
 
-# Version 3 f-string
-x = float(input("What's x? "))
-y = float(input("What's y? "))
+# # Version 3 f-string
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
 
-z = x / y
+# z = x / y
 
-print(f"{z:.2f}")
+# print(f"{z:.2f}")
+
+# Version 4 with return
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+
+def square(n):
+    # return n * n
+    # return pow(n, 2)
+    return n ** 2
+
+main()
