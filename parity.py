ed8835e7d2ee0972ef8ea359c7dd63bb0700c268
Rubi Leandro <rubi@brislandventures.com>
2023-08-06 10:04:19 +0400

    shorter

--- parity.py
+++ parity.py
@@ -8,7 +8,7 @@
 # else:
 #     print("Odd.")
 
-# Version 2
+# # Version 2
 def main():
     x = int(input("What's x? "))
     if is_even(x):
@@ -17,11 +17,20 @@ def main():
         print("Odd")
 
 
+# Version 2.0
+# def is_even(n):
+#     if n % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+# # Version 2.1 Pythonic    
+# def is_even(n):
+#     return True if n % 2 == 0 else False
+
+#version 2.2
 def is_even(n):
-    if n % 2 == 0:
-        return True
-    else:
-        return False
+    return n % 2 == 0
 
 
 main()
