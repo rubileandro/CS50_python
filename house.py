a119d9922e286d5428a228189ec3d84c518d83f0
Rubi Leandro <rubi@brislandventures.com>
2023-08-06 10:32:38 +0400

    Match

--- house.py
+++ house.py
@@ -1,9 +1,15 @@
 name = input("What's your name? ")
 
-if name == "Harry" or name == "Hermione" or name == "Ron":
-    print("Gryffindor")
-elif name == "Draco":
-    print("Slytherin")
-else:
-    print("Who?")
-
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    
+    # Catch all
+    case _:
+        print("Who?")
