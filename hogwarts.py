b1b71dac4e0bd597c7e7576d8ec8c91dd7f93749
Rubi Leandro <rubi@brislandventures.com>
2023-08-09 09:33:20 +0400

    looping over the dict

--- hogwarts.py
+++ hogwarts.py
@@ -5,7 +5,6 @@ students = {
   "Draco": "Slytherin",
   }
 
-print(students["Hermione"])
-print(students["Harry"])
-print(students["Ron"])
-print(students["Draco"])
+for student in students:
+    print(student, students[student])
+    
