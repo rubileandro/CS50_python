0c8bd3264168d711e1de65fcdc4890ab364be1c2
Rubi Leandro <rubi@brislandventures.com>
2023-08-10 14:03:47 +0400

    making it more compact

--- mario.py
+++ mario.py
@@ -2,17 +2,8 @@ def main():
     print_square(3)
 
 def print_square(size):
-
-    # For each row in square
     for i in range(size):
+        print("#" * size)
 
-        # For each brick in row
-        for j in range(size):
-
-            # Print brick
-            print("#", end="")
-
-            
-        print()
 
 main()
