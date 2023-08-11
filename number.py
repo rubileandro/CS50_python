939dd82f6e0d48b0282ec74aa16aee170b575be5
Rubi Leandro <rubi@brislandventures.com>
2023-08-11 14:43:05 +0400

    alternative main

--- number.py
+++ number.py
@@ -6,11 +6,9 @@ def main():
 def get_int():
     while True:
         try:
-            x = int(input("what's x? "))
+            return int(input("what's x? "))
         except ValueError:
             print("x is not an integer")
-        else:
-            return x
 
 
 main()
