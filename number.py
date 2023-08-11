85453f26815eb97031f641601b1394d0fb4b8afd
Rubi Leandro <rubi@brislandventures.com>
2023-08-11 14:41:28 +0400

    main version

--- number.py
+++ number.py
@@ -1,9 +1,16 @@
-while True:
-    try:
-        x = int(input("what's x? "))
-    except ValueError:
-        print("x is not an integer")
-    else:
-        break
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            x = int(input("what's x? "))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            return x
+
+
+main()
