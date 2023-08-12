e85cbe0de1959ee6d5cf612aede2a56c595a01a6
Rubi Leandro <rubi@brislandventures.com>
2023-08-12 14:23:10 +0400

    sys.exit for errors

--- name.py
+++ name.py
@@ -1,10 +1,8 @@
 import sys
 
-#Check for errors
 if len(sys.argv) < 2:
-    print("Too few arguments")
+    sys.exit("Too few arguments")
 elif len(sys.argv) > 2:
-    print("Too many arguments")
+    sys.exit("Too many arguments")
 
-#Print name tags
 print("hello, my name is", sys.argv[1])
