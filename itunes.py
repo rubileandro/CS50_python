2c15b484a0bedb666fc8c3da3eb8b6d20c84c860
Rubi Leandro <rubi@brislandventures.com>
2023-08-13 08:17:22 +0400

    Formatted more cleanly.

--- itunes.py
+++ itunes.py
@@ -1,3 +1,4 @@
+import json
 import requests 
 import sys
 
@@ -7,4 +8,4 @@ if len(sys.argv) != 2:
 
 # Request
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(response.json())
+print(json.dumps(response.json(), indent=2))
