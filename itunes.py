d914442f5068229a53733a9a94d31abc25d151c3
Rubi Leandro <rubi@brislandventures.com>
2023-08-13 07:59:21 +0400

    API call

---
+++ itunes.py
@@ -0,0 +1,10 @@
+import requests 
+import sys
+
+# Error checking
+if len(sys.argv) != 2:
+    sys.exit()
+
+# Request
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+print(response.json())
