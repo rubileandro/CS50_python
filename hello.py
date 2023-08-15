89d1cfd99b791f348db5ceb577156c694aefc6d7
Rubi Leandro <rubi@brislandventures.com>
2023-08-15 15:56:26 +0400

    hello() using return

--- hello.py
+++ hello.py
@@ -36,11 +36,11 @@
 # Option 4 main()
 def main():
     name = input("What's your name? ")
-    hello(name)
+    print(hello(name))
 
 
 def hello(to="world!"):
-    print("Hello,", to)
+    return f"hello, {to}"
 
 
 if __name__ == "__main__":
