9396f5f606845547a3c8a9367f9b56cafd87e02b
Rubi Leandro <rubi@brislandventures.com>
2023-08-15 16:47:20 +0400

    complete

---
+++ test/hello.py
@@ -0,0 +1,47 @@
+# Option 1
+# # What is your name?
+# name = input("What's your name? ")
+
+# # Remove whitespace & capitalize name
+# name = name.strip().title()
+
+# # Greet user
+# #print("Hello,", name, sep="???")
+# print(f"Hello, {name}")
+
+# # Option 2
+# # get name, remove whitepsace and capitalize
+# name = input("What's your name?").strip().title()
+
+# # Split name
+# first, name = name.split(" ")
+
+# # Greet user
+# #print("Hello,", name, sep="???")
+# print(f"Hello, {first}")
+
+
+# # overide default value \n
+# print("Hello,", end="")
+# print(name)
+
+# # Option 3
+# def hello(to="world!"):
+#     print("Hello,", to)
+
+# hello()
+# name = input("What's your name? ")
+# hello(name)
+
+# Option 4 main()
+def main():
+    name = input("What's your name? ")
+    print(hello(name))
+
+
+def hello(to="world!"):
+    return f"hello, {to}"
+
+
+if __name__ == "__main__":
+    main()
