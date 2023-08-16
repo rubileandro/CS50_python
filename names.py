83aedfe4a29a31f1fcb81c4ccd98c9cbd62b4f5e
Rubi Leandro <rubi@brislandventures.com>
2023-08-16 13:45:11 +0400

    sorting with loop

--- names.py
+++ names.py
@@ -1,2 +1,10 @@
-name = input("What's your name? ")
-print(f"hello, {name}")
+names = []
+
+for _ in range(3):
+    names.append(input("What's your name? "))
+
+    
+# Sort list in a loop
+for name in sorted(names):
+    print(f"hello, {name}")
+    
