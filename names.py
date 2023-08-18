9014552134b4d59a6de4693a91eba108fd6a1647
Rubi Leandro <rubi@brislandventures.com>
2023-08-18 17:22:51 +0400

    sorting

--- names.py
+++ names.py
@@ -1,3 +1,10 @@
-with open("names.txt", "r") as file:
+names = []
+
+with open("names.txt") as file:
     for line in file:
-        print("Hello,", line.rstrip())
+        # appending to the list in memory not the file
+        names.append(line.rstrip())
+
+
+for name in sorted(names):
+    print(f"Hello, {name}")
