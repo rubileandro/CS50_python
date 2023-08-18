391cbaf0497b6de03322c7c16efa32a268c063b4
Rubi Leandro <rubi@brislandventures.com>
2023-08-18 17:24:40 +0400

    More compact

--- names.py
+++ names.py
@@ -1,10 +1,4 @@
-names = []
-
 with open("names.txt") as file:
-    for line in file:
-        # appending to the list in memory not the file
-        names.append(line.rstrip())
-
-
-for name in sorted(names):
-    print(f"Hello, {name}")
+    for line in sorted(file):
+        print("Hello,", line.rstrip())
+        
