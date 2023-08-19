fda53be037c4ef80e37bce8f024c083282a1d8b3
Rubi Leandro <rubi@brislandventures.com>
2023-08-19 11:53:56 +0400

    using a lambda

--- students.py
+++ students.py
@@ -6,9 +6,7 @@ with open("students.csv") as file:
         student = {"name":name, "house":house}
         students.append(student)
 
-def get_house(student):
-    return student["house"]
-
-for student in sorted(students, key=get_house, reverse=True):
+# Lambda 
+for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
     
