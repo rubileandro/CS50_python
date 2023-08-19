b92e83cb62bea912166ca5c430dc2237d3732637
Rubi Leandro <rubi@brislandventures.com>
2023-08-19 11:45:11 +0400

    dict with sort and key

--- students.py
+++ students.py
@@ -3,10 +3,12 @@ students = []
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        student = {"name":name, "hosue":house}
+        student = {"name":name, "house":house}
         students.append(student)
 
+def get_house(student):
+    return student["house"]
 
-for student in students:
+for student in sorted(students, key=get_house, reverse=True):
     print(f"{student['name']} is in {student['house']}")
     
