cd30e093526485ab37559794874896c195405dac
Rubi Leandro <rubi@brislandventures.com>
2023-08-19 12:45:03 +0400

    using fieldnames

--- students.py
+++ students.py
@@ -4,6 +4,6 @@ name = input("What's your name? ")
 home = input("Where's your home? ")
 
 with open("students.csv", "a") as file:
-    writer = csv.writer(file)
-    writer.writerow([name,home])
-        
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
+    
