5a858f8043fb2034f1de12d3444a0c2295418ddc
Rubi Leandro <rubi@brislandventures.com>
2023-08-19 12:02:35 +0400

    using lambda

--- students.py
+++ students.py
@@ -1,12 +1,14 @@
+import csv
+
 students = []
     
 with open("students.csv") as file:
     for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name":name, "house":house}
+        name, home = line.rstrip().split(",")
+        student = {"name":name, "home":home}
         students.append(student)
 
 # Lambda 
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")
+    print(f"{student['name']} is in {student['home']}")
     
