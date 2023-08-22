c028d54d65989928f9068498337d9287a765aac1
Rubi Leandro <rubi@brislandventures.com>
2023-08-22 15:12:06 +0400

    ignoring case

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email? ").strip()
 
-if re.search(r"^\w+@\w+\.(com|edu|gov|net|org)$", email):
+if re.search(r"^\w+@\w+\.edu$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
