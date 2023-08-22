fab4dbe63ba9b74bb84ac7fe5b321e87d7e280c9
Rubi Leandro <rubi@brislandventures.com>
2023-08-22 16:42:09 +0400

    accounts for first/last name order

--- format.py
+++ format.py
@@ -1,8 +1,7 @@
 import re
 
 name = input("What's your name? ").strip()
-matches = re.search(r"^(.+), (.+)$", name)
+matches = re.search(r"^(.+), *(.+)$", name)
 if matches:
-    last, first = matches.groups()
-    name = f"{first} {last}"
+    name = matches.group(2) + " " + matches.group(1)
 print(f"Hello, {name}")
