a0bfdd82ec2b5ed69d92a7fb0f1441fa59eed30c
Rubi Leandro <rubi@brislandventures.com>
2023-08-23 16:27:57 +0400

    walrus operator

--- format.py
+++ format.py
@@ -1,7 +1,6 @@
 import re
 
 name = input("What's your name? ").strip()
-matches = re.search(r"^(.+), *(.+)$", name)
-if matches:
+if matches := re.search(r"^(.+), *(.+)$", name):
     name = matches.group(2) + " " + matches.group(1)
 print(f"Hello, {name}")
