bfe47d0be3b207eb2bd002035ceb23975b963bd9
Rubi Leandro <rubi@brislandventures.com>
2023-08-23 17:55:22 +0400

    re.sub()

--- twitter.py
+++ twitter.py
@@ -1,4 +1,6 @@
+import re
+
 url = input("URL: ").strip()
 
-username = url.replace("https://twitter.com/", "")
+username = re.sub(r"^(https?://)?(www\.)?twitter\.com/","", url)
 print(f"Username: {username}")
