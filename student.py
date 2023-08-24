a753c154cc2a46a7764266a9b7bb17a0c667f475
Rubi Leandro <rubi@brislandventures.com>
2023-08-24 13:31:17 +0400

    version 1

---
+++ student.py
@@ -0,0 +1,14 @@
+def main():
+    name = get_name()
+    house = get_house()
+    print(f"{name} from {house}")
+
+def get_name():
+    # Immediately returning the return value of input func call
+    return input("Name: ")
+
+def get_house():
+    return input("Name: ")
+
+if __name__ == "__main__":
+    main()
