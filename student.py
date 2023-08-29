c21fbd3c057fada69d3392f470cc02f29ccac512
Rubi Leandro <rubi@brislandventures.com>
2023-08-29 13:37:58 +0400

    patronus function

--- student.py
+++ student.py
@@ -1,31 +1,38 @@
 class Student:
-    def __init__ (self, name, house):
+    def __init__ (self, name, house, patronus):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {self.house}"
+    
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "🐎"
+            case "Otter":
+                return "🦦"
+            case "Jack Russell Terrier":
+                return "🐕"
+            case _:
+                return "🪄"
 
 def main():
     student = get_student()
-    print(student)
-# # Alternative
-# def get_name():
-#     # Immediately returning the return value of input func call
-#     return input("Name: ")
-
-# def get_house():
-#     return input("Name: ")
+    print("Expecto Patronum!")
+    print(student.charm())
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
+    patronus = input("Patronus: ")
     # Constructor call
-    return Student(name, house)
+    return Student(name, house, patronus)
 
 
 
