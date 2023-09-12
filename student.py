216e1e93e5e63fe45d8b78da72d1688771e6f0f1
Rubi Leandro <rubi@brislandventures.com>
2023-09-12 16:17:35 +0400

    More conventional

--- student.py
+++ student.py
@@ -7,37 +7,16 @@ class Student:
     def __str__(self):
         return f"{self.name} from {self.house}"
     
-    @property
-    def name(self):
-        return self._name
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
     
-    @name.setter
-    def name(self, name):
-        if not name:
-            raise ValueError("Missing name")
-        self._name = name
-
-    @property
-    def house(self):
-        return self._house
-    
-    @house.setter
-    def house(self, house):
-        if house not in ["Gryffindor", "Hufflepuff", "Racenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
-        self._house = house
 
 def main():
-    student = get_student()
+    student = Student.get()
     print(student)
-
-def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    # Constructor call
-    return Student(name, house)
-
-
 
 
 if __name__ == "__main__":
