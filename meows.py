ea9e344c038a3699bc9ef5625c682b9767b01523
Rubi Leandro <rubi@brislandventures.com>
2023-10-03 13:41:06 +0400

    fixed printing None

--- meows.py
+++ meows.py
@@ -1,8 +1,7 @@
-def meow(n: int) -> None:
-    for _ in range(n):
-        print("meow")
+def meow(n: int) -> str:
+    return "meow\n" * n
 
 
 number: int = int(input("Number: "))
 meows: str = meow(number)
-print(meows)
+print(meows, end="")
