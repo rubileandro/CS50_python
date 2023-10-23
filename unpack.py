68a1d6f743bc48e3302a2176522249345c23fa0b
Rubi Leandro <rubi@brislandventures.com>
2023-10-23 15:40:21 +0400

    asterisk unpacking

--- unpack.py
+++ unpack.py
@@ -1,3 +1,6 @@
-first, _ = input("What's your name? ").split(" ")
-print(f"hello, {first}")
- 
+def total(galleons, sickles, knuts):
+    return(galleons * 17 + sickles) * 29 + knuts
+
+coins = [100, 50, 25]
+
+print(total(*coins), "Knuts")
