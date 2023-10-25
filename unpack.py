fcb18f25e83d9ce1e8098081c8280ec983e0cd14
Rubi Leandro <rubi@brislandventures.com>
2023-10-25 16:06:09 +0400

    args kwargs practice

--- unpack.py
+++ unpack.py
@@ -1,6 +1,4 @@
-def total(galleons, sickles, knuts):
-    return(galleons * 17 + sickles) * 29 + knuts
+def f(*args, **kwargs):
+    print("Named:", kwargs)
 
-coins = {"galleons": 100, "sickles": 50, "knuts":25}
-
-print(total(**coins), "Knuts")
+f(galleons=100, sickles=50, knuts=25)
