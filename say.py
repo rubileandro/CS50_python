29ba2000e860f312234598c28c4890d4c4f6d42c
Rubi Leandro <rubi@brislandventures.com>
2023-10-27 11:40:49 +0400

    cowsay with text to voice

--- say.py
+++ say.py
@@ -1,17 +1,8 @@
-import sys
+import cowsay
+import pyttsx3
 
-from sayings import goodbye
-
-if len(sys.argv) == 2:
-    goodbye(sys.argv[1])
-
-
-# import cowsay
-# import sys
-
-# # Proactive error checking 
-# if len(sys.argv) == 2:
-#     # Cow
-#     cowsay.cow("hello," + sys.argv[1])
-#     # # T-rex
-#     # cowsay.trex("hello," + sys.argv[1])
+engine = pyttsx3.init()
+this = input("What's this? ")
+cowsay.cow(this)
+engine.say(this)
+engine.runAndWait()
