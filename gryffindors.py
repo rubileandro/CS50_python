4a4f2918a035beee4ada28a2f33b9279a895b8f9
Rubi Leandro <rubi@brislandventures.com>
2023-10-27 09:17:49 +0400

    filter version

--- gryffindors.py
+++ gryffindors.py
@@ -5,10 +5,10 @@ students = [
     {"name": "Draco", "house": "Slytherin"},
 ]
 
-gryffindors = [
-    student["name"] for student in students if student["house"] == "Gryffindor"
-]
+def is_gryffindor(s):
+    return s["house"] == "Gryffindor"
 
-for gryffindor in sorted(gryffindors):
-    print(gryffindor)
-    
+gryffindors = filter(is_gryffindor, students)
+
+for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+    print(gryffindor["name"])
